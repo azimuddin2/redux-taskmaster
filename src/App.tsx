d1db985ef548eca7561bc94f2427386425587b06
azimuddin2 <mohammadazimuddin274@gmail.com>
2025-01-04 22:44:04 +0600

    redux taskmaster project setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+
+  return (
+    <div>
+      <h1 className='text-5xl font-semibold text-center'>TaskMaster App</h1>
+    </div>
+  )
+}
+
+export default App
